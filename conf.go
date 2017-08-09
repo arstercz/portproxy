eea4ab523c7fbce1c8e266ef7f07cdae5f7a022b
arstercz <chenzhe07@gmail.com>
2017-08-09 16:11:03 +0800

    read real MySQL user and pass

--- conf.go
+++ conf.go
@@ -20,3 +20,15 @@ func get_backend_dsn(c *goconfig.ConfigFile) (dsn string, err error) {
 	}
 	return dsn, nil
 }
+
+func get_read_user(c *goconfig.ConfigFile) (user string, pass string, err error) {
+	user, err = c.GetString("onlineread", "user")
+	if err != nil {
+		return "", "", err
+	}
+	pass, err = c.GetString("onlineread", "pass")
+	if err != nil {
+		return user, "", err
+	}
+	return user, pass, nil
+}
